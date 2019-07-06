1fc9667cbf4dd74d7e66b8537302bc231680adc1
MOhan <themightyking117@gmail.com>
2019-07-06 23:59:34 +0530

    updated

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,7 +1,7 @@
 import React from 'react'
 import Layout from '../components/Layout'
 import ArticleList from '../components/ArticleList'
-
+import Helmet from 'react-helmet'
 import '../style.css'
 import 'bootstrap/dist/css/bootstrap.css'
 import { graphql } from 'gatsby';
